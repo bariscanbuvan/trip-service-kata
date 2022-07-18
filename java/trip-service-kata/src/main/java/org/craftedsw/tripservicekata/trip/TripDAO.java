ba1c76213be1d8b9048f40539c490a67a67cd59e
Barış Can BUVAN <bariscan.buvan@trendyol.com>
2022-07-18 17:20:55 +0300

    WIP apply seams and get rid of statics

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
@@ -7,7 +7,22 @@ import org.craftedsw.tripservicekata.user.User;
 
 public class TripDAO {
 
+	// make this class singleton
+	private static TripDAO instance = new TripDAO();
+
+	public TripDAO() {
+	}
+
+	public static TripDAO getInstance() {
+		return instance;
+	}
+
+	@Deprecated
 	public static List<Trip> findTripsByUser(User user) {
+		return instance.getTripsByUser(user);
+	}
+
+	public List<Trip> getTripsByUser(User user) {
 		throw new CollaboratorCallException(
 				"TripDAO should not be invoked on an unit test.");
 	}
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,99 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TripServiceTest {
-	
+
+    class TestableTripService extends TripService {
+
+        User user;
+
+        public TestableTripService(User user) {
+            this.user = user;
+        }
+
+        @Override
+        protected List<Trip> findTripsByUser(User user) {
+            return Collections.singletonList(new Trip());
+        }
+
+        @Override
+        protected User getLoggedUser() {
+            return user;
+        }
+    }
+
+    @Test
+    public void should_throw_UserNotLoggedInException_when_logged_user_is_null() {
+        // given
+
+
+        TestableTripService tripService = new TestableTripService(null);
+
+        // when
+        UserNotLoggedInException exception =
+                assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(new User()));
+
+        // then
+        assertNotNull(exception);
+    }
+
+    @Test
+    @Disabled
+    public void should_return_empty_trips_list_when_user_has_no_friend() {
+        // given
+        TestableTripService tripService = new TestableTripService(new User());
+
+        // when
+        List<Trip> trips = tripService.getTripsByUser(new User());
+
+        // then
+        assertEquals(0, trips.size());
+    }
+
+    @Test
+    @Disabled
+    public void should_return_trips_by_user() {
+        // given
+        User friend = new User();
+        User loggedUser = new User();
+        friend.addFriend(loggedUser);
+
+        class TestableTripService extends TripService {
+
+            User user;
+
+            public TestableTripService(User user) {
+                this.user = user;
+            }
+
+            @Override
+            protected List<Trip> findTripsByUser(User user) {
+                return Collections.singletonList(new Trip());
+            }
+
+            @Override
+            protected User getLoggedUser() {
+                loggedUser.addFriend(friend);
+                return loggedUser;
+            }
+        }
+
+        TripService tripService = new TestableTripService();
+
+        // when
+        List<Trip> trips = tripService.getTripsByUser(friend);
+
+        // then
+        assertTrue(trips.size() == 1);
+    }
 }
